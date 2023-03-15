005e515f5f808d88defec39944534b1f3f2991a5
Ritu <ritutec001@gmail.com>
2023-03-15 23:46:24 +0530

    Styling change

--- src/components/HeroSection/HomeSection.js
+++ src/components/HeroSection/HomeSection.js
@@ -4,13 +4,13 @@ const HomeSection = () => {
   return (
     <div className='home-section'>
        <div className="fimg">
-        <img src="./assert/Fshoe1.png" alt="" />
+        <img src="./assert/Fshoe2.png" alt="" />
        </div>
         
         <div className="images">
         <img src="./assert/Fshoe1.png" alt="" />
-        <img src="./assert/Fshoe1.png" alt="" />
-        <img src="./assert/Fshoe1.png" alt="" />
+        <img src="./assert/Fshoe2.png" alt="" />
+        <img src="./assert/Fshoe3.png" alt="" />
         </div>
 
         
